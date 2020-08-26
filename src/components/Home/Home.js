1d83da88ab51bbfa131821ae95b412674d75930e
c-cait <caitlinciaramella@gmail.com>
2020-08-26 11:22:12 -0700

    refactored Home to hook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,53 +1,42 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import './Home.css'
 import {getUser} from '../../redux/reducer';
 import Post from '../Post/Post'
 import axios from 'axios';
 
-class Home extends Component {
-    constructor(){
-        super();
-        this.state = {
-            posts: []
-        }
-        this.getAllPosts = this.getAllPosts.bind(this);
-    }
+function Home(props) {
+    const [posts, setPosts] = useState([])
 
-    getAllPosts() {
+   useEffect(() => {
+        props.getUser()
+        getAllPosts()
+    })
+
+    const getAllPosts = () => {
         axios.get('/api/sprout/posts')
         .then(res => {
-            this.setState({
-                posts: res.data
-            })
-            console.log('posts', this.state.posts)
+            setPosts(res.data)
+            console.log('posts', posts)
         })
         .catch(err => {
             console.log(err)
         })
     }
 
-    componentDidMount(){
-        this.props.getUser()
-        this.getAllPosts()
-    }
 
-    render(){
-        return(
-            <div className='home-container'>
-                <div>
-                    Welcome, {this.props.user.username}
-                </div>
-                <div className='home-posts-container'>
-                    {this.state.posts.map(elem => (
-                        <Post key={elem.post_id} post={elem} getAllPosts={this.getAllPosts}/>
-                     ))}
-                </div>
-            
+    return(
+        <div className='home-container'>
+            <div>
+                Welcome, {props.user.username}
             </div>
-            
-        )
-    }
+            <div className='home-posts-container'>
+                {posts.map(elem => (
+                    <Post key={elem.post_id} post={elem} getAllPosts={getAllPosts}/>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 const mapStateToProps = reduxState => reduxState
